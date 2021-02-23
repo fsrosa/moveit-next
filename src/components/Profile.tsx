2043b16ea88b0d8b9f4fedec3de7bf7f79ec6a11
FABRICIO ROSA <FABRICIO_ROSA@jabil.com>
2021-02-23 15:22:05 -0300

    nextjs fixed

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return(
+        <div>
+            <img src="https://github.com/fsrosa.png" alt="Fabrício Rosa"/>
+            <div>
+                <strong>Fabrício Rosa</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import Head from 'next/head'
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
 
 export default function Home() {
   return (
     <div className='container'>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap" rel="stylesheet" />
-      </Head>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
